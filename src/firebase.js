25054edbaf9f0b4c5c1fe29e5743ec61574cc133
Molly Seeley <mseeley89@gmail.com>
2019-09-09 10:59:37 -0400

    setup: started firebase configurations

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig = firebasei.initializeApp({
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messageSenderId: '',
+  apiId: '',
+})
+
+export { firebaseConfig as firebase }
