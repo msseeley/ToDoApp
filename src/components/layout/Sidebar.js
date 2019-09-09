afe7f4bb4150d9f6c575f6dc62466811ee4a2eb8
Molly Seeley <mseeley89@gmail.com>
2019-09-09 10:49:30 -0400

    preliminary sidebar layout

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons'
+import { FaChevronDown, FaInbox, FaRegCalendarAlt, FaRegCalendar } from 'react-icons/fa'
 
 export const Sidebar = () => (
   <div className="sidebar" data-testid="sidebar">
     <ul className="sidebar_generic" >
-      <li>Inbox</li>
-      <li>Today</li>
-      <li>Next 7 Days</li>
+      <li><span><FaInbox /></span> <span>Inbox</span></li>
+      <li><span><FaRegCalendar /></span> <span>Today</span></li>
+      <li><span><FaRegCalendarAlt /></span> <span>Next 7 Days</span></li>
     </ul>
+    <div className="sidebar__middle">
+      <span><FaChevronDown /></span>
+      <h2>Projects</h2>
+    </div>
+    <ul className="sidebar__projects">WILL CONTAIN PROJECTS</ul>
+    {/* Add Project Component Here */}
   </div>
 )
