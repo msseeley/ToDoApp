768f9c3beb050a906fe4af9598b7686523e3eb5e
Molly Seeley <mseeley89@gmail.com>
2019-09-17 18:58:25 -0400

    set up: firebase configurations

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = firebasei.initializeApp({
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messageSenderId: '',
-  apiId: '',
+  apiKey: "AIzaSyCTZ5tXtTTskFMTRKnnrHNpTNL5L2G5Wf8",
+  authDomain: "react-hooks-tutorial-todoist.firebaseapp.com",
+  databaseURL: "https://react-hooks-tutorial-todoist.firebaseio.com",
+  projectId: "react-hooks-tutorial-todoist",
+  storageBucket: "react-hooks-tutorial-todoist.appspot.com",
+  messagingSenderId: "1088128086377",
+  appId: "1:1088128086377:web:2d5a09095a7113f5084df8"
 })
 
 export { firebaseConfig as firebase }
